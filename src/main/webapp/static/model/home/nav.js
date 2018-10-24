0cf5c3adda35f3f94b163985e83118288a41a770
joicewq <1072057614@qq.com>
2018-10-24 14:47:54 +0800

    栏目信息更新

--- src/main/webapp/static/model/home/nav.js
+++ src/main/webapp/static/model/home/nav.js
@@ -63,13 +63,49 @@
 //			         },
 			         {
 			        	 id:"004",
-			        	 text:"通知公告",
+			        	 name:"通知公告",
 			        	 url:"",
-			        	 child:[
+			        	 children:[
 			        	        {
 			        	        	id:"004_1",
-			        	        	text:"双公示1",
-			        	        	url:"",
+			        	        	name:"双公示1",
+			        	        },{
+			        	        	id:"004_2",
+			        	        	name:"双公示2",
+			        	        	children:[
+			      			        	        {
+			    			        	        	id:"004_1",
+			    			        	        	name:"双公示1",
+			    			        	        },{
+			    			        	        	id:"004_2",
+			    			        	        	name:"双公示2",
+			    			        	        },
+			    			        	        {
+			    			        	        	id:"004_3",
+			    			        	        	name:"双公示3",
+			    			        	        }
+			    			        	       ]
+			        	        },
+			        	        {
+			        	        	id:"004_3",
+			        	        	name:"双公示3",
+			        	        }
+			        	       ]
+			         },
+			         {
+			        	 id:"004",
+			        	 name:"通知公告",
+			        	 children:[
+			        	        {
+			        	        	id:"004_1",
+			        	        	name:"双公示1",
+			        	        },{
+			        	        	id:"004_2",
+			        	        	name:"双公示2",
+			        	        },
+			        	        {
+			        	        	id:"004_3",
+			        	        	name:"双公示3",
 			        	        }
 			        	       ]
 			         },
@@ -95,35 +131,34 @@
 		if(data["navList"][curUrl]){
 			var activeItemId=data["navList"][curUrl];
 		}
-		var navData=data["navData"]//queryColumn();
+		var navData=queryColumn();
 		function createNav(data,$container,$parentLi){
+			
 			$.each(data,function(i,ele){
 				var thisObj=$(ele)[0];
 				var $li=$("<li></li>");
-				var url=thisObj["url"]==null?"javascript:void(0);":thisObj["url"];
-				var $a = $("<a></a>").text(thisObj["text"]).attr("href",url);
-				if(typeof thisObj["child"] != "undefined"){
+//				var url=thisObj["url"]==null?"":thisObj["url"];
+				var $a = $("<a></a>").text(thisObj["name"]).attr("href","");
+				if(typeof thisObj["children"] != "undefined"){
 					var $ul = $("<ul></ul>").addClass("hide child-nav");
 					$li.addClass("has-child").append($ul);
-					$.each(thisObj["child"],function(i,eleindex){
-						var child = $(eleindex)[0];
-						createNav(thisObj["child"],$ul,$li);
-					})
+					createNav(thisObj["children"],$ul,$li);
 				}
 				$li.prepend($a).appendTo($container);
 				
 				
 			})
 		}
-		createNav(navData,$("#nav"));		
-		
-		$("#nav").find("li").hover(function(){
-			if($(this).children("ul").length>0){
-				$(this).children("ul").show().removeClass("hide");
-			}
-		},function(){
-			if($(this).children("ul").length>0){
-				$(this).children("ul").hide().addClass("hide");				
+	
+		$("#nav").on("mouseover mouseout","li",function(mouseover){
+			if(mouseover.type!=="mouseout"){
+				if($(this).children("ul").length>0){
+					$(this).children("ul").show().removeClass("hide");
+				}
+			}else{
+				if($(this).children("ul").length>0){
+					$(this).children("ul").hide().addClass("hide");				
+				}
 			}
 		})
 		
@@ -145,7 +180,6 @@
 					columnNode:"0",//通过顶级栏目ID查询所以子集
 					orderBySort: "1"
  			}
- 			debugger;
  			$.ajax({
  				url: "/portal/queryNewColumn", //url  action是方法的名称
  				type: 'post',
@@ -156,8 +190,7 @@
  					if(msg.code == "1"){
  						var data = msg.data;
  						var dataItem = list2tree(data, "id", "pColumnId");
- 						console.log("dataItem",dataItem);
- 						return dataItem;
+ 						createNav(dataItem,$("#nav"));		
  					}else{
  						layer.msg("操作失败",{icon:2});
  					}
@@ -174,24 +207,7 @@
  		 function list2tree(list, idKey, parentKey){
  			var result = [];
  			var hash = {};
-
  			//把数组遍历出用ID作为KEY的JSON对象
- 			/*list.forEach(ele => {
- 				hash[ele[idKey]] = ele;
- 			});
- 			list.forEach(ele => {
- 				var _id = ele[idKey]; //当前节点的id
- 				var _parentKey = hash[_id][parentKey]; //当前节点的父节点KEY
-
- 				if(_parentKey && hash[_parentKey]) { //如果有父节点KEY，并且父节点存在
- 					if(!hash[_parentKey].children) { //如果当前节点还没有子节点，为其添加一个空的子节点
- 						hash[_parentKey].children = [];
- 					}
- 					hash[_parentKey].children.push(ele); //把当前节点添加到父节点的子节点路径上
- 				} else {
- 					result.push(ele); //添加没有父节点KEY或者没有父节点存在的节点到根节点上
- 				}
- 			});*/
 			  $.each(list, function(i,ele){     
 				  hash[ele[idKey]] = ele;
 			  });    
@@ -212,55 +228,7 @@
  			
  			return result;
  		}
- 		/**
- 		 * 退出
- 		 */
- 		function exit(){
- 			$.ajax({
- 				url: "/login/delSession", //url  action是方法的名称
- 				type: 'GET',
- 				dataType: "json", //可以是text，如果用text，返回的结果为字符串；如果需要json格式的，可是设置为json
- 				//contentType: "application/json; charset=utf-8",
- 				success: function(msg) {
- 					if(msg.result == 1){
- 						layer.alert("操作成功",{},function(){
- 							location.href="/salt/index"
- 						});
- 					}else{
- 						layer.msg("操作失败",{icon:2});
- 					}
- 				},
- 				error: function(msg) {
- 		        	layer.msg("操作失败",{icon:2});
- 		        }
- 			
- 			});
- 		}
- 		
- 		$('#alogout').on('click', function() {
- 		 
- 			$.ajax({
- 				url: "/login/delsession", //url  action是方法的名称
- 				type: 'GET',
- 				dataType: "json", //可以是text，如果用text，返回的结果为字符串；如果需要json格式的，可是设置为json
- 				//contentType: "application/json; charset=utf-8",
- 				success: function(msg) {
-// 					console.info("aa:"+msg)
- 					if(msg.result == 1){
- 						layer.alert("操作成功",{},function(){
- 							location.href="/salt/index"
- 						});
- 					}else{
- 						layer.msg("操作失败",{icon:2});
- 					}
- 				},
- 				error: function(msg) {
- 		        	layer.msg("操作失败1",{icon:2});
- 		        }
- 			
- 			});
-
- 		});
+ 
  		
  		/**
  		 * 搜索
@@ -277,18 +245,5 @@
  			console.log(str);
  			//location.href="/salt/serach?keyName="+keyName;
  		});*/
-
- 		// 系统通知
-		var loginId = $('#loginId').val();
- 		if(loginId){
- 			console.log(1)
- 			/*setInterval(function(){
- 				$.post(url,{},function(data){
-	 				if(data){
-	 					$("#userName").after('<a href="/vipcenter/index"><i class="fa fa-bell"></i>('+data+')</a>')
-	 				}
-	 			})
- 			},10000)*/
- 		}
 
 })
